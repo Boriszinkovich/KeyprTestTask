c5214bd518febdfaeab5e94699b8a72b9df707fe
Borys Zinkovych <bzinkovych@blackberry.com>
2018-11-08 19:17:15 +0200

    Initial Commit

---
+++ KeyprTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyprTestTask
+//
+//  Created by Borys Zinkovych on 11/8/18.
+//  Copyright © 2018 bzink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
